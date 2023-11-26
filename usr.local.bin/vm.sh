066ce44855c4552c20257f3f1c2a586179762371
Frederick Key Abell III <fkabell@localhost.localdomain>
2023-11-26 01:59:04 -0600

    fixed broken usage(), and " -> '

--- usr.local.bin/vm.sh
+++ usr.local.bin/vm.sh
@@ -23,21 +23,24 @@ iface="$(awk '$2 == 00000000 { print $7, $1 }' /proc/net/route | sort | \
 	awk 'NR == 1 { print $2 }')"
 
 usage() {
-	printf "%s" "Syntax error: Usage: $(basename "$0") "
-	if [ "$(find "$dir" -type d 2>/dev/null | wc -l)" -eq 2 ]; then
-		printf "%s" "$(basename $dir/*)"
-	elif [ "$(find "$dir" -type d 2>/dev/null | wc -l)" -ge 3 ]; then
-		printf "%s" "{"
-		for vmdir in "$dir"/*; do
-			[ -d "$vmdir" ] && printf "%s" "$(basename "$vmdir") | "
-		done | sed "s/...$/}/"
+	printf '%s' "Syntax error: Usage: $(basename "$0" 2>/dev/null) "
+	subdirs="$(find /var/vm/* -exec sh -c '[ -f "$0"/*.iso ] || \
+		[ -f "$0"/disk.qcow2 ] || [ -f "$0"/disk2.qcow2 ]' '{}' \
+		\; -print | sed 's/\/var\/vm\///g')"
+	if [ "$(printf '%s' "$subdirs" | wc -w)" -eq 1 ]; then
+		printf '%s' "$subdirs"
+	elif [ "$(printf '%s' "$subdirs" | wc -w)" -ge 2 ]; then
+		printf '%s' '{'
+		for vmdir in $subdirs; do
+			printf '%s' "$vmdir | "
+		done | sed 's/...$/}/'
 	fi
-	printf "%s\n" " [delay]"
+	printf '%s\n' ' [delay]'
 	delayexit 255
 }
 
 privdrop() {
-	if [ X"$1" = X"qemu" ]; then
+	if [ X"$1" = X'qemu' ]; then
 		# QEMU needs to escalate privs for bridge helper
 		# to create tun device and needs to keep $DISPLAY
 		shift
@@ -76,7 +79,7 @@ ckperm() {
 				log "$permseverity" \
 					"User $user can't read or write to $file."
 				lsperm "$file"
-				if [ X"$permseverity" = X"fatal" ]; then
+				if [ X"$permseverity" = X'fatal' ]; then
 					delayexit "$status"
 				else
 					return "$status"
@@ -85,7 +88,7 @@ ckperm() {
 				log "$permseverity" \
 					"User $user can't write to $file."
 				lsperm "$file"
-				if [ X"$permseverity" = X"fatal" ]; then
+				if [ X"$permseverity" = X'fatal' ]; then
 					delayexit "$status"
 				else
 					return "$status"
@@ -95,20 +98,20 @@ ckperm() {
 		if ! privdrop [ -r "$file" ]; then
 			log "$permseverity" "User $user can't read $file."
 			lsperm "$file"
-			if [ X"$permseverity" = X"fatal" ]; then
+			if [ X"$permseverity" = X'fatal' ]; then
 				delayexit "$status"
 			else
 				return "$status"
 			fi
 		fi
 	else
-		[ X"$existseverity" = X"info" ] || \
+		[ X"$existseverity" = X'info' ] || \
 			if [ -z "$file" ]; then
 				log "$existseverity" "No .iso file exist."
 			else
 				log "$existseverity" "File $file does not exist."
 			fi
-			if [ X"$existseverity" = X"fatal" ]; then
+			if [ X"$existseverity" = X'fatal' ]; then
 				delayexit "$status"
 			else
 				return "$status"
@@ -117,20 +120,20 @@ ckperm() {
 }
 
 lsperm() {
-	printf "\t%s" ">> "
+	printf '\t%s' '>> '
 	ls -l "$1" | cut -d ' ' -f 1,3,4
 }
 
 log() {
 	case "$1" in
 		info)
-			printf "%s\n" "$2"
+			printf '%s\n' "$2"
 		;;
 		warning)
-			printf "%s %s\n" "Warning:" "$2" >&2
+			printf '%s %s\n' 'Warning:' "$2" >&2
 		;;
 		fatal)
-			printf "%s %s\n" "Fatal:" "$2" >&2
+			printf '%s %s\n' 'Fatal:' "$2" >&2
 		;;
 	esac
 }
@@ -140,14 +143,14 @@ log() {
 # (for use with dmenu(1) graphical program)
 delay="$2"
 delayexit() {
-	if [ X"$delay" = X"delay" ]; then
+	if [ X"$delay" = X'delay' ]; then
 		read REPLY
 	fi
 	exit "$1"
 }
 
-if [ X"$(whoami)" != X"root" ]; then
-	log fatal "Must be superuser."
+if [ X"$(whoami)" != X'root' ]; then
+	log fatal 'Must be superuser.'
 	delayexit 254
 fi
 
@@ -156,7 +159,7 @@ rm -rf "$dir"/.cache
 if [ -d "$dir" ]; then
 	if [ "$(find "$dir" -type d | wc -l)" -gt 1 ]; then
 		if [ -n "$1" ]; then
-			if [ X"$1" = X"delay" ]; then
+			if [ X"$1" = X'delay' ]; then
 				usage
 			elif [ -d "$dir/$1" ]; then
 				vm="$dir/$1"
@@ -208,12 +211,12 @@ ip addr flush dev virbr0
 ip addr add 10.0.0.1/30 dev virbr0
 ip -6 addr add fe80::1/64 dev virbr0
 ip link set virbr0 up
-if [ X"$(sysctl net.ipv4.ip_forward)" = X"net.ipv4.ip_forward = 0" ]; then
+if [ X"$(sysctl net.ipv4.ip_forward)" = X'net.ipv4.ip_forward = 0' ]; then
 	sysctl net.ipv4.ip_forward=1 >/dev/null 2>&1
 	routing4=wasoff
 fi
 if [ X"$(sysctl net.ipv6.conf.all.forwarding)" = \
-	X"net.ipv6.conf.all.forwarding = 0" ]; then
+	X'net.ipv6.conf.all.forwarding = 0' ]; then
 	sysctl net.ipv6.conf.all.forwarding=1 >/dev/null 2>&1
 	routing6=wasoff
 fi
@@ -252,16 +255,16 @@ log info "Undoing network config changes"
 # Re-enable VPN here
 ip link set virbr0 down
 brctl delbr virbr0
-[ X"$routing4" = X"wasoff" ] && sysctl net.ipv4.ip_forward=0 >/dev/null 2>&1
-[ X"$routing6" = X"wasoff" ] && sysctl net.ipv6.conf.all.forwarding=0 \
+[ X"$routing4" = X'wasoff' ] && sysctl net.ipv4.ip_forward=0 >/dev/null 2>&1
+[ X"$routing6" = X'wasoff' ] && sysctl net.ipv6.conf.all.forwarding=0 \
 	>/dev/null 2>&1
 
 if [ "$status" -eq 139 ]; then
 	if [ -z "$DISPLAY" ]; then
-		log info "\$DISPLAY is not set."
+		log info '$DISPLAY is not set.'
 	else
-		log info "Likely either \$DISPLAY is incorrectly set or"
-		log info "root is denied by X access control (xhost(1))."
+		log info 'Likely either $DISPLAY is incorrectly set or'
+		log info 'root is denied by X access control (xhost(1)).'
 	fi
 fi
 
